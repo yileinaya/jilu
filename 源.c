af1e9ab9b918ae74b360b9350d3261e545770b07
yileinaya <138093168+yileinaya@users.noreply.github.com>
2023-06-30 21:26:22 +0800

    Update 源.c

--- 源.c
+++ 源.c
@@ -1,20 +1,13 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//加法运算（这行文字前面的斜杠不能去）
-//#include<stdio.h>
-//int main()
-//{
-//	int a = 0;
-//	int b = 0;
-//	int sum = 0;
-//	scanf("%d %d", &a, &b);
-//	sum = a + b;
-//	printf("sum = %d", sum);
-//
-//	return 0;
-//}
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 int main()
 {
-	printf("sjksdjfkla");
+	int a = 0;
+	int b = 0;
+	int sum = 0;
+	scanf("%d %d", &a, &b);
+	sum = a + b;
+	printf("sum = %d", sum);
+
 	return 0;
 }
