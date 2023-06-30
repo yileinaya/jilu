c850b9c73a8c21bb83256b0d1772f969d6e48b3e
yileinaya <138093168+yileinaya@users.noreply.github.com>
2023-06-30 21:24:41 +0800

    Add files via upload

---
+++ 源.c
@@ -0,0 +1,20 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//加法运算（这行文字前面的斜杠不能去）
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int sum = 0;
+//	scanf("%d %d", &a, &b);
+//	sum = a + b;
+//	printf("sum = %d", sum);
+//
+//	return 0;
+//}
+#include<stdio.h>
+int main()
+{
+	printf("sjksdjfkla");
+	return 0;
+}
